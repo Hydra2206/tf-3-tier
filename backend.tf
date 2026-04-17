2096073a6e0b5b4064be3761d8cd6fbc322b374d
Hydra2206 <mittu270318@gmail.com>
2026-04-17 19:06:37 +0530

    test

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-backend-bket"
+    key            = "s3-backend-bket/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lock_file = true
+  }
+}
